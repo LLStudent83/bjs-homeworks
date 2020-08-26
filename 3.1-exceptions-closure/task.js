fcf0f4831cfa674186d6e603c6dc0fe2bf96314a
Dmitry Prigulnov <prigulnov@mail.ru>
2020-08-26 15:08:09 +0500

    add 3.1

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,17 +1,63 @@
-function parseCount(strPars){
-  const pars = Number.parseInt( strPars, 10 );
-  if (pars == NaN) {
-    throw "Невалидное значение"
+function parseCount(strPars) {
+  const pars = Number.parseInt(strPars, 10);
+  if (isNaN(pars)) {
+    const parseCountErr = new Error("Невалидное значение");
+    throw parseCountErr;
   };
-return pars;
-};
-parseCount("kjn");
-
-const divider = (a,b) => {
-  if(b == 0)
-  throw"Ошибка деления на 0";
-return a/b;
+  return pars;
 };
 
-console.log(divider(1,2));// 0.5
-console.log(divider(1,0));// "Ошибка деления на 0";
+
+function validateCount(strPars) {
+  try {
+    parseCount(strPars);
+    return parseCount(strPars);
+  } catch (ErrPars) {
+  return ErrPars;
+  }
+};
+
+class Triangle {
+  constructor(a, b, c) {
+    if (((a + b) <= c) || ((a + c) <= b)) {
+      const errTr = new Error("Треугольник с такими сторонами не существует");
+      throw errTr;
+    };
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  };
+
+  getPerimeter() {
+    let a = this.a;
+    let b = this.b;
+    let c = this.c;
+    const p = a + b + c;
+    return p;
+  };
+  getArea() {
+    let a = this.a;
+    let b = this.b;
+    let c = this.c;
+    const hp = (a + b + c) / 2;
+    const s = (Math.floor((Math.sqrt(hp * ((hp - a) * (hp - b) * (hp - c)))) * 1000)) / 1000;
+    return s;
+  };
+};
+
+function getTriangle(a, b, c) {
+  try {
+    let triangle2 = new Triangle(a, b, c);
+    return triangle2;
+  } catch(err) {
+    const triangleErr = {
+      getPerimeter: function() {
+        return "Ошибка! Треугольник не существует";
+      },
+      getArea: function() {
+        return "Ошибка! Треугольник не существует";
+      }
+    };
+    return triangleErr;
+  }
+}
