4c9d3d308356a64aa48b3195982afd63ba0f975e
Dmitry Prigulnov <prigulnov@mail.ru>
2020-09-15 22:24:19 +0500

    add 3.3 15/09

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -1,26 +1,94 @@
-class AlarmClock{
-    constructor(){
-      this.alarmCollection = [];
-      this.timerId = undefined  //хранение id таймена
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.timerId = null; //хранение id таймена
+  }
+  addClock(timeStart, callback, id) {
+    if (!id) {
+      throw new Error("id не передан");
     }
-    addClock(timeStart, ){
-  
+    if (this.alarmCollection.find((item) => item.id === id)) {
+      console.error("Будильник с таким id уже существует");
+      return;
+    }
+    this.alarmCollection.push({ time: timeStart, callback: callback, id: id });
+    return this.alarmCollection;
+  }
+  removeClock(id) {
+    //удаляет определённый звонок
+    let remClAlarmCollection = this.alarmCollection.filter(
+      (item) => item.id != id
+    );
+    console.log(remClAlarmCollection);
+    if (remClAlarmCollection.length === this.alarmCollection.length) {
+      return false;
+    } else {
+      this.alarmCollection = remClAlarmCollection;
+      return true;
     }
   }
-  let ac1 = new AlarmClock();
-  console.log(ac1);
-  ac1.timerId = 125;
-  console.log(ac1);
-  
-  class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-      this.name = name;
-      this.releaseDate = releaseDate;
-      this.pagesCount = pagesCount;
-      this.state = 100;
-      this.type = null;
+  getCurrentFormattedTime() {
+    return new Date().toLocaleTimeString().slice(0, -3);
+  }
+  start() {
+    const checkClock = (alarm) => {
+      if (alarm.time === this.getCurrentFormattedTime()) {
+        alarm.callback();
+        this.removeClock(alarm.id);
+      }
+    };
+
+    if (!this.timerId) {
+      this.timerId = setInterval(
+        () => this.alarmCollection.forEach(checkClock),
+        1000
+      );
     }
   }
-  let book = new PrintEditionItem("Зима", 2000, 1);
-  console.log(book);
-  
+  stop() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+  printAlarms() {
+    console.log(
+      `печать всех будильников в количестве ${this.alarmCollection.length}`
+    );
+    this.alarmCollection.forEach((item) =>
+      console.log(`Будильник № ${item.id} заведен на ${item.time}`)
+    );
+  }
+  clearAlarms() {
+    this.stop();
+    return (this.alarmCollection = []);
+  }
+}
+function testCase(nameAlarm) {
+  nameAlarm = new AlarmClock();
+  nameAlarm.addClock(
+    "22:18",
+    () => console.log("Я будильниек 1 вали на работу"),
+    1
+  );
+  nameAlarm.addClock(
+    "22:19",
+    () => {
+      console.log("Я будильниек 2 вали на работу");
+      nameAlarm.removeClock(2);
+    },
+    2
+  );
+  nameAlarm.addClock(
+    "22:22",
+    () => {
+      console.log("Я будильниек 2 вали на работу");
+      nameAlarm.stop();
+      nameAlarm.printAlarms();
+      nameAlarm.clearAlarms();
+    },
+    3
+  );
+  nameAlarm.start();
+}
+testCase("alarm01");
